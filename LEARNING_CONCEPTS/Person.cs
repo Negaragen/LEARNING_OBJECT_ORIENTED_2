883403ad4d8a098f386efd05745dc677ab7408eb
Mr. Dariush Tasdighi <DariushT@GMail.com>
2016-10-27 17:27:16 +0330

    Init Commit

---
+++ LEARNING_CONCEPTS/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace LEARNING_CSHARP
+{
+	public class Person
+	{
+		public int Age;
+		public string FullName;
+
+		public void ShowInfo()
+		{
+			System.Console.WriteLine("I'm {0} and {1} years old.", FullName, Age);
+		}
+	}
+}
---
+++ LEARNING_CONCEPTS/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace LEARNING_CSHARP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// **********
+			//Person P1;
+
+			//P1.Age = 20;
+			// **********
+
+			// **********
+			//Person P1;
+			//P1 = new Person();
+
+			//P1.ShowInfo();
+			// **********
+
+			// **********
+			//Person P1 = new Person();
+
+			//P1.Age = 20;
+			//P1.FullName = "Ali Reza Alavi";
+
+			//P1.ShowInfo();
+			// **********
+
+			// **********
+			//Person P1 = new Person();
+
+			//P1.Age = 20;
+			//P1.FullName = "Ali Reza Alavi";
+
+			//P1.ShowInfo();
+
+			//Person P2  = new Person();
+
+			//P2.Age = 30;
+			//P2.FullName = "Sara Ahmadi";
+
+			//P1.ShowInfo();
+			// **********
+
+			// **********
+			//Person P1 = new Person();
+
+			//P1.Age = 20;
+			//P1.FullName = "Ali Reza Alavi";
+
+			//P1.ShowInfo();
+
+			////Person P2;
+			//Person P2 = new Person();
+
+			//P2 = P1;
+
+			//P2.Age = 30;
+
+			//P1.ShowInfo();
+			// **********
+
+			System.Console.Write("Press [ENTER] To Exit...");
+			System.Console.ReadLine();
+		}
+	}
+}
